ea9a1ff113364c977993c22edf676c8d263573ac
Nasreen khan <160172100+Nasreenkh@users.noreply.github.com>
2024-05-16 01:06:35 +0500

    This is 45 Question Assignment Q-25.
    This is 45 Question Assignment Q-25.

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+/*question no 25. Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called
+alien_color and assign it a value of 'green', 'yellow', or 'red'.
+" Write an if statement to test whether the alien’s color is green. If it is, print a message that the player
+just earned 5 points.
+" Write one version of this program that passes the if test and another that fails. (The version that fails
+will have no output.) */
+//Passing version:
+let alien_color = 'green';
+// If statement to test whether the alien's color is green
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+}
+//Failing version:
+let alien1_color = 'red';
+// If statement to test whether the alien's color is green
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+}
